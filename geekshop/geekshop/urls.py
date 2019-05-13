d30b89ecf636eaaa3cfbf0f3b7537dcd6602ed4f
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-05-13 16:40:33 +0300

    service static files when debug

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('order', include('ordersapp.urls', namespace='order')),
 ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
