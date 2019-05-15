99c643e205dc5d378a38ab12b0af3260a560884a
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-05-15 22:06:11 +0300

    add receiver for quantity update

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -6,6 +6,8 @@ from django.forms import inlineformset_factory
 
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
 
 from basketapp.models import Basket
 from ordersapp.models import Order, OrderItem
@@ -72,7 +74,7 @@ class OrderItemsUpdate(UpdateView):
         return Order.objects.filter(user=self.request.user)
     
     def get_context_data(self, **kwargs):
-        data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
+        data = super().get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
 
         if self.request.POST:
@@ -96,7 +98,7 @@ class OrderItemsUpdate(UpdateView):
         if self.object.get_total_cost() == 0:
             self.object.delete()
 
-        return super(OrderItemsUpdate, self).form_valid(form)
+        return super().form_valid(form)
 
 
 class OrderDelete(DeleteView):
@@ -118,3 +120,20 @@ def order_forming_complete(request, pk):
     order.status = Order.SENT_TO_PROCEED
     order.save()
     return HttpResponseRedirect(reverse('ordersapp:orders_list'))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if instance.pk:
+        instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
