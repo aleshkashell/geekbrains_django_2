9a9e3185b7573afad904e354f03da3c60c24ad6a
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-05-27 22:57:55 +0300

    fix ajax and caching

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -7,9 +7,9 @@ app_name = 'mainapp'
 
 urlpatterns = [
    path('', mainapp.products, name='index'),
-   path('category/<int:pk>/', cache_page(3600)(mainapp.products), name='category'),
+   path('category/<int:pk>/', mainapp.products, name='category'),
    path('category/<int:pk>/ajax/', cache_page(3600)(mainapp.products_ajax)),
-   path('category/<int:pk>/page/<int:page>/', cache_page(3600)(mainapp.products), name='page'),
+   path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
    path('category/<int:pk>/page/<int:page>/ajax/', cache_page(3600)(mainapp.products_ajax)),
    path('product/<int:pk>/', mainapp.product, name='product'),
 ]
--- geekshop/static/js/main_scripts.js
+++ geekshop/static/js/main_scripts.js
@@ -2,14 +2,12 @@ $( document ).on( 'click', '.details a', function(event) {
    if (event.target.hasAttribute('href')) {
        var link = event.target.href + 'ajax/';
        var link_array = link.split('/');
-       //console.log(link);
-       //console.log(link_array[4]);
        if (link_array[4] === 'category') {
            $.ajax({
                url: link,
                success: function (data) {
-                   //console.log('success !!!!!!!!!!!!');
-                   $('.details').html(data.result);
+                  // console.log(data.result)
+                  $('.details').html(data.result);
                },
            });
 
