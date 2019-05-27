2c46c1fdf9138bc7bf0865682ea0f07a109b2160
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-05-27 21:43:20 +0300

    Fix ajax urls

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
    path('category/<int:pk>/', cache_page(3600)(mainapp.products), name='category'),
    path('category/<int:pk>/ajax/', cache_page(3600)(mainapp.products_ajax)),
    path('category/<int:pk>/page/<int:page>/', cache_page(3600)(mainapp.products), name='page'),
-   path('category/<int:pk>/page/<int:page>/ajax/', cache_page(3600)(mainapp.products)),
+   path('category/<int:pk>/page/<int:page>/ajax/', cache_page(3600)(mainapp.products_ajax)),
    path('product/<int:pk>/', mainapp.product, name='product'),
 ]
